714fc53994c7fdebc7516a4a5557c0db8d747ccb
ttheusLima <ttheus.rlima@gmail.com>
2023-02-28 13:00:47 -0300

    Removido a propriedade email

--- Api/Livraria_Api/Entities/Autor.cs
+++ Api/Livraria_Api/Entities/Autor.cs
@@ -8,9 +8,5 @@ namespace Livraria_Api.Entities
         [Required]
         [StringLength(50)]
         public string Nome { get; set; }
-        [Required]
-        [EmailAddress]
-        [StringLength(100)]
-        public string Email { get; set; }
     }
 }
