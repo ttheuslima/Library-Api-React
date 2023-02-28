3a7babb5c133bf5a52709a4b0e1b01b7c8585eb0
ttheusLima <ttheus.rlima@gmail.com>
2023-02-28 13:26:55 -0300

    Criado o contexto do banco de dados e registrado o contêiner para injeção de dependência

--- Api/Livraria_Api/Program.cs
+++ Api/Livraria_Api/Program.cs
@@ -1,11 +1,20 @@
+using Livraria_Api.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<LivrariaContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => 
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Livraria.Api", Version = "v1" } );
+});
 
 var app = builder.Build();
 
