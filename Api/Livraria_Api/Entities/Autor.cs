8d3fab2e13f2916c088b2eb02ba5d28a90eda9df
ttheusLima <ttheus.rlima@gmail.com>
2023-02-28 12:17:26 -0300

    Adicionado nova propriedade em Autor, colocado DataAnnotations nas entidades

--- Api/Livraria_Api/Entities/Autor.cs
+++ Api/Livraria_Api/Entities/Autor.cs
@@ -1,13 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Livraria_Api.Entities
 {
     public class Autor
     {
         public int AutorId { get; set; }
+        [Required]
+        [StringLength(50)]
         public string Nome { get; set; }
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
+        public string Email { get; set; }
     }
 }
--- Api/Livraria_Api/Entities/Livro.cs
+++ Api/Livraria_Api/Entities/Livro.cs
@@ -1,16 +1,19 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Livraria_Api.Entities
 {
     public class Livro
     {
         public int LivroId { get; set; }
+        [Required]
+        [StringLength(50)]
         public string Nome { get; set; }
+        [Required]
         public DateTime DataLancamento { get; set; }
+        [Required]
         public int Edicao { get; set; }
+        [Required]
+        [StringLength(50)]
         public string Editora { get; set; }
         public Autor Autor { get; set; }
     }
