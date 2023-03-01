df10a3cb5636c395f2ca16941844aa293debe984
ttheusLima <ttheus.rlima@gmail.com>
2023-03-01 11:44:08 -0300

    Criado migrations e populado as tabelas

---
+++ Api/Livraria_Api/Migrations/20230228172847_CriacaoTabelasLivroAutor.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Livraria_Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriacaoTabelasLivroAutor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Autores",
+                columns: table => new
+                {
+                    AutorId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Autores", x => x.AutorId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Livros",
+                columns: table => new
+                {
+                    LivroId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    DataLancamento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Edicao = table.Column<int>(type: "int", nullable: false),
+                    Editora = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    AutorId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Livros", x => x.LivroId);
+                    table.ForeignKey(
+                        name: "FK_Livros_Autores_AutorId",
+                        column: x => x.AutorId,
+                        principalTable: "Autores",
+                        principalColumn: "AutorId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Livros_AutorId",
+                table: "Livros",
+                column: "AutorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Livros");
+
+            migrationBuilder.DropTable(
+                name: "Autores");
+        }
+    }
+}
---
+++ Api/Livraria_Api/Migrations/20230301143039_PopularTabelas.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Livraria_Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularTabelas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Autores",
+                columns: new[] { "AutorId", "Nome" },
+                values: new object[,]
+                {
+                    { 1, "JONATHAN SWIFT" },
+                    { 2, "ROBERT LOUIS STEVERSON" },
+                    { 3, "J. K. Rowling" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Livros",
+                columns: new[] { "LivroId", "AutorId", "DataLancamento", "Edicao", "Editora", "Nome" },
+                values: new object[,]
+                {
+                    { 1, 1, new DateTime(1726, 10, 28, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, "OXFORD BOOKWORMS", "Gulliver's Travels" },
+                    { 2, 2, new DateTime(1986, 1, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), 3, "RIchmond READERS", "Dr Jekyll & Mr Hyde" },
+                    { 3, 3, new DateTime(1997, 6, 26, 0, 0, 0, 0, DateTimeKind.Unspecified), 2, "Editora Rocco", "Harry Potter and the Philosopher's Stone" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Livros",
+                keyColumn: "LivroId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Livros",
+                keyColumn: "LivroId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Livros",
+                keyColumn: "LivroId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Autores",
+                keyColumn: "AutorId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Autores",
+                keyColumn: "AutorId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Autores",
+                keyColumn: "AutorId",
+                keyValue: 3);
+        }
+    }
+}
