73d7afd5582fc3fe0df13d406b051d3c9eb7b2d5
ttheusLima <ttheus.rlima@gmail.com>
2023-03-03 11:38:44 -0300

    Criado response padrão de erro

---
+++ Api/Livraria_Api/Models/ErrorResponseVm.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Livraria_Api.Models
+{
+    public class ErrorResponseVm
+    {
+        public ErrorResponseVm()
+        {
+            TraceId = Guid.NewGuid().ToString();
+            Errors = new List<ErrorDetailsVm>();
+        }
+
+        public ErrorResponseVm(string logref, string message)
+        {
+            TraceId = Guid.NewGuid().ToString();
+            Errors = new List<ErrorDetailsVm>();
+            AddError(logref, message);
+        }
+
+        public string TraceId { get; private set; }
+        public List<ErrorDetailsVm> Errors { get; private set; }
+
+        public class ErrorDetailsVm
+        {
+            public ErrorDetailsVm(string logref, string message)
+            {
+                Logref = logref;
+                Message = message;
+            }
+
+            public string Logref { get; private set; }
+
+            public string Message { get; private set; }
+        }
+
+        public void AddError(string logref, string message)
+        {
+            Errors.Add(new ErrorDetailsVm(logref, message));
+        }
+    }
+}
