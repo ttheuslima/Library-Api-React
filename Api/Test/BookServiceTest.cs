9bcde0b8d678494f0f615bee2f0c6773348941b0
ttheusLima <ttheus.rlima@gmail.com>
2023-05-15 16:38:22 -0300

    Adicionado novos pacotes e recomeçado

---
+++ Api/Test/BookServiceTest.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Library_Api.Interfaces;
+using Moq;
+using NUnit.Framework;
+
+namespace Test
+{
+    public class BookServiceTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
