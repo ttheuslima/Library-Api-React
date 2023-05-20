312003bad5149458dd11256d0637c1418342d896
ttheusLima <ttheus.rlima@gmail.com>
2023-05-20 17:55:51 -0300

    Teste não terminado, porém com resultados promissores

--- Api/Test/BookServiceTest.cs
+++ Api/Test/BookServiceTest.cs
@@ -2,7 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Library_Api.Contexts;
+using Library_Api.Entities;
 using Library_Api.Interfaces;
+using Library_Api.Services;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using NUnit.Framework;
 
@@ -10,16 +14,47 @@ namespace Test
 {
     public class BookServiceTest
     {
+        private Mock<DbSet<Book>> _dbSet;
+        private Mock<LibraryContext> _mockContext;
+        private IBookService _service;
+
         [SetUp]
         public void Setup()
         {
-            
+            var sampleData = new List<Book>
+            {
+                // Initialize your test data
+                new Book { BookId = 1,
+                    Name = "Gulliver's Travels",
+                    YearLauch = "1726",
+                    Edition = 1,
+                    PublishingCompany = "Pearson English",
+                    UrlWebImg = "https://m.media-amazon.com/images/I/51W6w1PrkbL._SX384_BO1,204,203,200_.jpg",
+                    Price = 59M, Author = new Author { AuthorId = 1,
+                    Name = "Jonathan Swift" } }
+                
+            }.AsQueryable();
+
+            _dbSet = new Mock<DbSet<Book>>();
+            var asyncEnumerable = new TestAsyncEnumerable<Book>(sampleData);
+            _dbSet.As<IAsyncEnumerable<Book>>().Setup(m => m.GetAsyncEnumerator(default)).Returns(asyncEnumerable.GetAsyncEnumerator(default));
+            _dbSet.As<IQueryable<Book>>().Setup(x => x.Provider).Returns(sampleData.Provider);
+            _dbSet.As<IQueryable<Book>>().Setup(x => x.Expression).Returns(sampleData.Expression);
+            _dbSet.As<IQueryable<Book>>().Setup(x => x.ElementType).Returns(sampleData.ElementType);
+            _dbSet.As<IQueryable<Book>>().Setup(x => x.GetEnumerator()).Returns(sampleData.GetEnumerator());
+
+            _mockContext = new Mock<LibraryContext>();
+            _mockContext.Setup(c => c.Books).Returns(_dbSet.Object);
+
+            _service = new BookService(_mockContext.Object);
         }
 
         [Test]
-        public void Test1()
+        public async Task Test1AsyncGetTotalBooksAsync_Should_Return_Total_Books()
         {
-            Assert.Pass();
+             var result = await _service.Get_Books();
+
+             Assert.IsNotNull(result);
         }
     }
 }
