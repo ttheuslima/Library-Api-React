0687bdd1513219c6a638c3668355a60810662040
ttheusLima <ttheus.rlima@gmail.com>
2023-05-20 17:54:28 -0300

    implementado a QueryProvider e abilitada para poder utilizar no mocking

---
+++ Api/Test/TestAsyncEnumerable.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class TestAsyncEnumerable<T> : EnumerableQuery<T>, IAsyncEnumerable<T>, IQueryable<T>
+    {
+        public TestAsyncEnumerable(IEnumerable<T> enumerable)
+            : base(enumerable)
+        { }
+
+        public TestAsyncEnumerable(Expression expression)
+            : base(expression)
+        { }
+
+        IQueryProvider IQueryable.Provider => new TestAsyncQueryProvider<T>(this);
+
+        public IAsyncEnumerator<T> GetAsyncEnumerator(CancellationToken token)
+        {
+            return new TestAsyncEnumerator<T>(this.AsEnumerable().GetEnumerator());
+        }
+    }
+}
---
+++ Api/Test/TestAsyncEnumerator.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class TestAsyncEnumerator<T> : IAsyncEnumerator<T>
+    {
+        private readonly IEnumerator<T> enumerator;
+
+        public TestAsyncEnumerator(IEnumerator<T> enumerator)
+        {
+            this.enumerator = enumerator;
+        }
+
+        public T Current => this.enumerator.Current;
+
+        public ValueTask DisposeAsync()
+        {
+            return new ValueTask(Task.Run(() => this.enumerator.Dispose()));
+        }
+
+        public ValueTask<bool> MoveNextAsync()
+        {
+            return new ValueTask<bool>(this.enumerator.MoveNext());
+        }
+    }
+}
