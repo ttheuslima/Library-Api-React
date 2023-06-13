40a05c80bb4f1707aba336085a45e620685e9652
ttheusLima <ttheus.rlima@gmail.com>
2023-06-13 12:46:58 -0300

    Criado arquivos bases para desenvolver autenticação JWT

---
+++ Api/Library_Api/ViewModels/LoginModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library_Api.ViewModels
+{
+    public class LoginModel
+    {
+        
+    }
+}
